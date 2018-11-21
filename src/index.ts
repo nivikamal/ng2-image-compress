4cc793eed6f3809b9f7a6d9d23e354f3d1f36ab0
nivikamal <nivikamal@gmail.com>
2018-11-21 10:22:57 -0600

    Delete index.ts

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './ResizeOptions';
-export * from './CompressImage';
-export * from './ImageCompressService';
-export * from './imageUtilityService';
-export * from './CompressImageModule';
